94c3e480ac14264aa27d22ae21ba8ca211afb191
Scott Sorensen <scott.sorensen@octanner.com>
2014-10-12 23:06:18 -0600

    fix bug

--- app/controllers/translation_controller.rb
+++ app/controllers/translation_controller.rb
@@ -9,10 +9,10 @@ class TranslationController < ApplicationController
     render :json => t, :callback => params[:callback]
   end
 
-  def translateEnglish english, language
+  def translateEnglish(english, language)
     if language == "pirate"
       {pirate: Pirate.new.translate(english)}
-    if language == "yoda"
+    elsif language == "yoda"
       {yoda: Yoda.new.translate(english)}
     elsif language == "piglatin"
       {piglatin: Piglatin.new.translate(english)}
@@ -21,44 +21,5 @@ class TranslationController < ApplicationController
     else
       {error: "invalid language: '#{language}'"}
     end
-  end
-
-  def translate2Pirate english
-    response = HTTParty.get( URI::escape "http://www.pir.to/api/translate?english=#{english}" )
-    {pirate: response.body}
-  end
-
-  def translate2PigLatin english
-    # credit:  http://codereview.stackexchange.com/questions/40189/pig-latin-translator-in-ruby-and-rspec
-    {piglatin: english.gsub!(/\w+/) { |word| pig(word) } }
-  end
-
-  def translate2Dug english
-    {dug: english.split.first(5).join(" ") + ".... SQUIRREL!"}
-  end
-
-  private
-
-  def pig(word)
-    replace_consonants_to_end_of_word(word)
-    append_n_to_last_letter_y(word) or append_y_to_last_letter_vowel(word)
-    recapitalize(word)
-    word += 'ay'
-  end
-
-  def replace_consonants_to_end_of_word(word)
-    word.concat(word.slice!(/^[^aeiou]*/i || ""))
-  end
-
-  def append_n_to_last_letter_y(word)
-    word.gsub!(/y$/, "yn")
-  end
-
-  def append_y_to_last_letter_vowel(word)
-    word.gsub!(/([aeiou])$/, '\1y')
-  end
-
-  def recapitalize(word)
-    word.capitalize! if word.downcase!
   end
 end
