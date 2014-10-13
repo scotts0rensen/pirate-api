571ed41a7594b51e62beab101236fed39cf5513b
Scott Sorensen <scott.sorensen@octanner.com>
2014-10-12 19:37:16 -0600

    add pig latin

--- app/controllers/translation_controller.rb
+++ app/controllers/translation_controller.rb
@@ -9,11 +9,30 @@ class TranslationController < ApplicationController
     render :json => {pirate: pirate}, :callback => params[:callback]
   end
 
-  private
-
   def translate2Pirate english
     response = HTTParty.get( URI::escape "http://www.pir.to/api/translate?english=#{english}" )
     response.body
   end
 
+  def translate2PigLatin english
+    english.split.collect { |w| pig(w) }.join(" ")
+  end
+
+  private
+
+  def pig(word)
+    leadingCap = word =~ /^A-Z?/
+    word.downcase!
+    res = case word
+      #when vowels word + way
+      when /^aeiouy/
+        word+"way"
+      #otherwise, start from the back to concatanete with the first letter and add ay
+      when /^([^aeiouy]+)(.*)/
+        $2 + $1 + "ay"
+      else
+        word
+    end
+    leadingCap ? res.capitalize : res
+  end
 end
